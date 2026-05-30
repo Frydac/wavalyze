4a9cf46ecbe91d76e773cb25f68ce26271f0c8e0
Emile Vrijdags <emile.vrijdags@newauro.com>
2026-05-30 11:44:36 +0200

    fix: sample_pos_is_hovered takes zoom into account

--- src/model/hover_info.rs
+++ src/model/hover_info.rs
@@ -8,9 +8,10 @@ pub struct HoverInfo {
 }
 
 impl HoverInfo {
-    pub fn sample_pos_hovered(&self, sample_pos_x: f64) -> bool {
+    pub fn sample_pos_hovered(&self, sample_pos_x: f64, samples_per_pixel: f64) -> bool {
+        let pixels_per_half_sample = 0.5 / samples_per_pixel;
         self.sample_pos_x.is_some_and(|x| {
-            crate::math::compare::near_absolute(x as f32, sample_pos_x as f32, 0.1)
+            crate::math::compare::near_absolute(x as f32, sample_pos_x as f32, pixels_per_half_sample as f32)
         })
     }
 }
@@ -24,12 +25,10 @@ pub enum HoverInfoE {
 
 impl HoverInfoE {
     // Given a sample position, are we hovering over that sample?
-    // NOTE: utility function, as we have transformed our sample indices to screen positions and
-    // don't store the sample ix (we probably should, it is not that much data, but this works too)
-    pub fn sample_pos_is_hovered(&self, sample_pos_x: f64) -> bool {
+    pub fn sample_pos_is_hovered(&self, sample_pos_x: f64, samples_per_pixel: f64) -> bool {
         match self {
             HoverInfoE::NotHovered => false,
-            HoverInfoE::IsHovered(hover_info) => hover_info.sample_pos_hovered(sample_pos_x),
+            HoverInfoE::IsHovered(hover_info) => hover_info.sample_pos_hovered(sample_pos_x, samples_per_pixel),
         }
     }
 }
